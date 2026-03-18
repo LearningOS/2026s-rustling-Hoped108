0cace8978d4ec0aed285898dc06f1e1cc51c1818
Hoped108 <19806868812@163.com>
2026-03-18 15:19:24 +0800

    string and enum

--- exercises/enums/enums1.rs
+++ exercises/enums/enums1.rs
@@ -2,11 +2,14 @@
 //
 // No hints this time! ;)
 
-// I AM NOT DONE
 
 #[derive(Debug)]
 enum Message {
     // TODO: define a few types of messages as used below
+    Quit,
+    Move { x: i32,y: i32},
+    Write(String),
+    ChangeColor(i32,i32,i32),
 }
 
 fn main() {
