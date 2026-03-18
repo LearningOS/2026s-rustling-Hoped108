80915ac2adc4c041de3313fb0f9eaa08fabd0874
Hoped108 <19806868812@163.com>
2026-03-18 11:52:53 +0800

    first commit

--- exercises/functions/functions4.rs
+++ exercises/functions/functions4.rs
@@ -10,11 +10,11 @@
 
 
 fn main() {
-    let original_price = 51;
-    println!("Your sale price is {}", sale_price(original_price));
+    let mut original_price = 51;
+    println!("Your sale price is not{}", sale_price(original_price));
 }
 
-fn sale_price(price: i32) -> i32 {
+fn sale_price(mut price: i32) -> i32 {
     if is_even(price) {
         price - 10
     } else {
