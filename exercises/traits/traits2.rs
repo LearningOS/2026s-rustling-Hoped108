63f7f0f7cfd94a986c7d2b74fe279b3d9145cc61
Hoped108 <19806868812@163.com>
2026-03-25 16:30:35 +0800

    update_trait_error

--- exercises/traits/traits2.rs
+++ exercises/traits/traits2.rs
@@ -14,7 +14,7 @@ trait AppendBar {
 }
 
 // TODO: Implement trait `AppendBar` for a vector of strings.
-impl<String> AppendBar for Vec<String>{
+impl AppendBar for Vec<String>{
     fn append_bar(mut self) -> Self{
         self.push(String::from("Bar"));
         self
